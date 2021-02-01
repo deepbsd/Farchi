7a425fc36a41ebda6ee6e000897c42f6e5f15ee6
David S. Jackson <deepbsd@yahoo.com>
2021-02-01 15:14:12 -0500

    more on simplest.sh

--- simplest.sh
+++ simplest.sh
@@ -19,3 +19,23 @@ BASE_SYSTEM=( base base-devel linux linux-headers linux-firmware dkms vim iwd )
 devel_stuff=( git nodejs npm npm-check-updates ruby )
 printing_stuff=( system-config-printer foomatic-db foomatic-db-engine gutenprint cups cups-pdf cups-filters cups-pk-helper ghostscript gsfonts )
 multimedia_stuff=( brasero sox cheese eog shotwell imagemagick sox cmus mpg123 alsa-utils cheese )
+
+
+###  Just use cfdisk to partition drive
+cfdisk "$IN_DEVICE"    # for non-EFI VM: /boot 512M; / 13G; Swap 2G; Home Remainder
+
+#####  Format filesystems
+mkfs.ext4 /dev/sda1    # /boot
+mkfs.ext4 /dev/sda2    # /
+mkswap /dev/sda3       # swap partition
+mkfs.ext4 /dev/sda4    # /home
+
+#### Mount filesystems
+mount /dev/sda2 /mnt
+mkdir /mnt/boot && mount /dev/sda1 /mnt/boot
+swapon /dev/sda3
+mkdir /mnt/home && mount /dev/sda4 /mnt/home
+
+
+
+

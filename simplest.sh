03d15f1ac19bdae207f2f7881d71743f8f8ef483
David S. Jackson <deepbsd@yahoo.com>
2021-02-04 17:38:17 -0500

    updated simplest.sh

--- simplest.sh
+++ simplest.sh
@@ -13,6 +13,13 @@ TIME_ZONE="America/New_York"
 LOCALE="en_US.UTF-8"
 FILESYSTEM=ext4
 
+use_bcm4360(){ return 0; }
+
+if $(use_bcm4360) ; then
+    WIRELESSDRIVERS="broadcom-wl-dkms"
+else
+    WIRELESSDRIVERS=""
+fi
 
 BASE_SYSTEM=( base base-devel linux linux-headers linux-firmware dkms vim iwd )
 
@@ -20,9 +27,28 @@ devel_stuff=( git nodejs npm npm-check-updates ruby )
 printing_stuff=( system-config-printer foomatic-db foomatic-db-engine gutenprint cups cups-pdf cups-filters cups-pk-helper ghostscript gsfonts )
 multimedia_stuff=( brasero sox cheese eog shotwell imagemagick sox cmus mpg123 alsa-utils cheese )
 
+### Check of reflector is done
+clear
+echo "Waiting until reflector has finished updating mirrorlist..."
+while true; do
+    pgrep -x reflector &>/dev/null || break
+    echo -n '.'
+    sleep 2
+done
 
+### Test internet connection
+clear
+echo "Testing internet connection..."
+$(ping -c 3 archlinux.org &>/dev/null) || (echo "Not Connected to Network!!!" && exit 1)
+echo "Good!  We're connected!!!" && sleep 3
 ###  Just use cfdisk to partition drive
 cfdisk "$IN_DEVICE"    # for non-EFI VM: /boot 512M; / 13G; Swap 2G; Home Remainder
+
+## Check time and date before installation
+timedatectl set-ntp true
+echo && echo "Date/Time service Status is . . . "
+timedatectl status
+sleep 4
 
 #####  Format filesystems
 mkfs.ext4 /dev/sda1    # /boot
@@ -36,6 +62,92 @@ mkdir /mnt/boot && mount /dev/sda1 /mnt/boot
 swapon /dev/sda3
 mkdir /mnt/home && mount /dev/sda4 /mnt/home
 
+###  Install base system
+clear
+echo && echo "Press any key to continue to install BASE SYSTEM..."; read empty
+pacstrap /mnt "${BASE_SYSTEM[@]}"
+echo && echo "Base system installed.  Press any key to continue..."; read empty
 
+# GENERATE FSTAB
+echo "Generating fstab..."
+genfstab -U /mnt >> /mnt/etc/fstab
 
+## SET UP TIMEZONE AND LOCALE
+clear
+echo && echo "setting timezone to $TIME_ZONE..."
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/"$TIME_ZONE" /etc/localtime
+arch-chroot /mnt hwclock --systohc --utc
+arch-chroot /mnt date
+echo && echo "Here's the date info, hit any key to continue..."; read td_yn
 
+## SET UP LOCALE
+clear
+echo && echo "setting locale to $LOCALE ..."
+arch-chroot /mnt sed -i "s/#$LOCALE/$LOCALE/g" /etc/locale.gen
+arch-chroot /mnt locale-gen
+echo "LANG=$LOCALE" > /mnt/etc/locale.conf
+export LANG="$LOCALE"
+cat /mnt/etc/locale.conf
+echo && echo "Here's your /mnt/etc/locale.conf. Type any key to continue."; read loc_yn
+
+## HOSTNAME
+clear
+echo && echo "Setting hostname..."; sleep 3
+echo "$HOSTNAME" > /mnt/etc/hostname
+
+cat > /mnt/etc/hosts <<HOSTS
+127.0.0.1      localhost
+::1            localhost
+127.0.1.1      $HOSTNAME.localdomain     $HOSTNAME
+HOSTS
+
+echo && echo "/etc/hostname and /etc/hosts files configured..."
+echo "/etc/hostname . . . "
+cat /mnt/etc/hostname 
+echo "/etc/hosts . . ."
+cat /mnt/etc/hosts
+echo && echo "Here are /etc/hostname and /etc/hosts. Type any key to continue "; read etchosts_yn
+
+## SET ROOT PASSWD
+clear
+echo "Setting ROOT password..."
+arch-chroot /mnt passwd
+
+## INSTALLING MORE ESSENTIALS
+clear
+echo && echo "Enabling dhcpcd, sshd and NetworkManager services..." && echo
+arch-chroot /mnt pacman -S git openssh networkmanager dhcpcd man-db man-pages
+arch-chroot /mnt systemctl enable dhcpcd.service
+arch-chroot /mnt systemctl enable sshd.service
+arch-chroot /mnt systemctl enable NetworkManager.service
+echo && echo "Press any key to continue..."; read empty
+
+## ADD USER ACCT
+clear
+echo && echo "Adding sudo + user acct..."
+sleep 2
+arch-chroot /mnt pacman -S sudo bash-completion sshpass
+arch-chroot /mnt sed -i 's/# %wheel/%wheel/g' /etc/sudoers
+arch-chroot /mnt sed -i 's/%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
+echo && echo "Please provide a username: "; read sudo_user
+echo && echo "Creating $sudo_user and adding $sudo_user to sudoers..."
+arch-chroot /mnt useradd -m -G wheel "$sudo_user"
+echo && echo "Password for $sudo_user?"
+arch-chroot /mnt passwd "$sudo_user"
+
+## INSTALL WIFI
+$(use_bcm4360) && arch-chroot /mnt pacman -S "$WIRELESSDRIVERS"
+[[ "$?" -eq 0 ]] && echo "Wifi Driver successfully installed!"; sleep 5
+
+## No X Today!!
+
+## INSTALL GRUB
+clear
+echo "Installing grub..." && sleep 4
+arch-chroot /mnt pacman -S grub os-prober
+## We're not checking for EFI; We're assuming MBR
+arch-chroot /mnt grub-install "$IN_DEVICE"
+echo "mbr bootloader installed..."
+
+echo "configuring /boot/grub/grub.cfg..."
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
